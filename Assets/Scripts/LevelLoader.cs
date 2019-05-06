9afcb0401f4e5adda10cf24b48c26e2a30d2ea87
Gavin Luo <luoxiangyu528@gmail.com>
2019-05-05 23:28:40 -0400

    Initial Commit, create Glitch Garden git repo.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private int waitSeconds = 3;
+
+    private int currentSceneIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(LoadNextSceneWithDelay());
+    }
+
+    private IEnumerator LoadNextSceneWithDelay()
+    {
+        yield return new WaitForSeconds(waitSeconds);
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
